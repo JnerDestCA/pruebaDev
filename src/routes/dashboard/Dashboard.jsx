4033a119ecf952c4a35fe5b9761a09aad391be5e
JnerDestCA <cabelloalvaradojenner@gmail.com>
2026-04-20 12:46:41 -0500

    cambios pc

--- src/routes/dashboard/Dashboard.jsx
+++ src/routes/dashboard/Dashboard.jsx
@@ -1,7 +1,68 @@
-function Dashboard(){
-    return(
+function Dashboard() {
+    return (
         <>
-            <h1>Hola desde el dashboad</h1>
+            <section class="flex flex-col p-6 ">
+                <div class="pb-6">
+                    <span class="text-blue-900 font-bold">Dashboard</span>
+                    <div class="flex flex-row w-full grid grid-cols-4 gap-8">
+                        <div class="flex flex-col shadow shadow-gray-400 p-4 rounded-xl mt-4">
+                            <span class="text-gray-500">Ingresos</span>
+                            <p class="text-gray-600 text-xl font-bold p-1">S/460.00</p>
+                            <div class="flex flex-row gap-3 items-center">
+                                <div class="p-1 show shadow bg-green-100 rounded-lg">
+                                    <span class="text-green-600">+ S/50k</span>
+                                </div>
+                                <span class="text-gray-500">Desde el mes pasado</span>
+                            </div>
+                        </div>
+                        <div class="flex flex-col shadow shadow-gray-400 p-4 rounded-xl mt-4">
+                            <span class="text-gray-500">Numero de Clientes</span>
+                            <p class="text-gray-600 text-xl font-bold p-1">4789</p>
+                            <div class="flex flex-row gap-3 items-center">
+                                <div class="p-1 show shadow bg-red-100 rounded-lg">
+                                    <span class="text-red-600">-30 clientes</span>
+                                </div>
+                                <span class="text-gray-500">Desde el mes pasado</span>
+                            </div>
+                        </div>
+                        <div class="flex flex-col shadow shadow-gray-400 p-4 rounded-xl mt-4">
+                            <span class="text-gray-500">Inversion realizada</span>
+                            <p class="text-gray-600 text-xl font-bold p-1">S/5460.00k</p>
+                            <div class="flex flex-row gap-3 items-center">
+                                <div class="p-1 show shadow bg-green-100 rounded-lg">
+                                    <span class="text-green-600">+ S/32k</span>
+                                </div>
+                                <span class="text-gray-500">Desde el mes pasado</span>
+                            </div>
+                        </div>
+                        <div class="flex flex-col shadow shadow-gray-400 p-4 rounded-xl mt-4">
+                            <span class="text-gray-500">Relacion de ganancia</span>
+                            <p class="text-gray-600 text-xl font-bold p-1">S/460.00</p>
+                            <div class="flex flex-row gap-3 items-center">
+                                <div class="p-1 show shadow bg-red-100 rounded-lg">
+                                    <span class="text-red-600">- S/50k</span>
+                                </div>
+                                <span class="text-gray-500">Desde el mes pasado</span>
+                            </div>
+                        </div>
+                    </div>
+
+
+                </div>
+
+                <div>
+                    <span class="text-blue-900 font-bold">Historial de Ventas</span>
+                </div>
+                <div class="flex">
+                    <div>
+                        <span class="text-blue-900 font-bold">Historial de Ventas</span>
+                    </div>
+                    <div>
+                        <span class="text-blue-900 font-bold">Historial de Ventas</span>
+                    </div>
+                </div>
+
+            </section>
         </>
     )
 }
--- src/routes/paginasWeb/PaginasWeb.jsx
+++ src/routes/paginasWeb/PaginasWeb.jsx
@@ -1,8 +1,44 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faEnvelope,faPencil,faEye,faTrashCan    } from '@fortawesome/free-solid-svg-icons'
 function PaginasWeb() {
     return (
         <>
-            <section>
-                <h1>Holas mundo desde la paginas web</h1>
+            <section class="p-6">
+                <div>
+                    <span class="text-blue-900 font-bold">Paginas Webs</span>
+                    <div class="shadow w-full flex justify-center mt-4 overflow-auto rounded-lg">
+                        <table class="w-full border border-gray-200">
+                            <thead>
+                                <tr>
+                                    <th>N</th>
+                                    <th>Categoria</th>
+                                    <th>Descripcion</th>
+                                    <th>Fecha</th>
+                                    <th>Categoria</th>
+                                    <th>Acciones</th>
+                                </tr>
+                            </thead>
+                            <tbody> 
+                                <tr>
+                                    <td class="p-3 text-sm text-gray-700">1</td>
+                                    <td class="p-3 text-sm text-gray-700 flex justify-center">Recursos graficos</td>
+                                    <td class="p-3 text-sm text-gray-700">Recursos graficos que se usaran en paginas que se deberan usar a su momento
+                                        Recursos graficos que se usaran en web
+                                    </td>
+                                    <td class="p-3 text-sm text-gray-700 flex justify-center">15/04/24 - 6:30 hrs.</td>
+                                    <td class="p-3 text-sm text-gray-700">
+                                        <span class="p-1.5 text-xs font-medium uppercase text-orange-600 bg-orange-100 rounded-lg flex justify-center">Imagenes</span>
+                                    </td>
+                                    <td class="p-3 text-sm text-gray-700 flex justify-between">
+                                        <FontAwesomeIcon icon={faPencil} />
+                                        <FontAwesomeIcon icon={faEye} />
+                                        <FontAwesomeIcon icon={faTrashCan} />
+                                    </td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
             </section>
         </>
     )
